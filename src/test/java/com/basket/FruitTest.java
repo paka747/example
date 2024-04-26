2e882ea6c831caaa5e6ee7858ca72c8ad8e27aa8
Andrzej Smoliński <andrzej.smolinski@allegro.pl>
2024-04-26 11:17:21 +0200

    added kotlin variant of cat/fruit task

--- src/test/java/com/basket/FruitTest.java
+++ src/test/java/com/basket/FruitTest.java
@@ -9,7 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class FruitTest {
 
   FruitProcessor processor = new FruitProcessor();
-  //FruitProcessor processor = new FruitProcessorKt();
+//  FruitProcessorKt processor = new FruitProcessorKt();
 
   @Test
   void shouldFindLetters() {
